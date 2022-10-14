50079d031d126b704dfab40e00ae4807a1ccfa68
Ogster08 <104634399+Ogster08@users.noreply.github.com>
2022-10-14 18:02:13 +0100

    removed comments

--- Affine-solver/Affine-cipher/Program.cs
+++ Affine-solver/Affine-cipher/Program.cs
@@ -55,10 +55,9 @@
 
         public static Dictionary<string, int> TextFrequency(string testText)
         {
-            //dictionary for counting letters
             var characterCount = new Dictionary<string, int>() { { "a", 0 }, { "b", 0 }, { "c", 0 }, { "d", 0 }, { "e", 0 }, { "f", 0 }, { "g", 0 }, { "h", 0 }, { "i", 0 }, { "j", 0 }, { "k", 0 }, { "l", 0 }, { "m", 0 }, { "n", 0 }, { "o", 0 }, { "p", 0 }, { "q", 0 }, { "r", 0 }, { "s", 0 }, { "t", 0 }, { "u", 0 }, { "v", 0 }, { "w", 0 }, { "x", 0 }, { "y", 0 }, { "z", 0 } };
 
-            foreach (char c in testText) { characterCount[c.ToString()]++; } //increse the count of that character by 1
+            foreach (char c in testText) { characterCount[c.ToString()]++; }
             return characterCount;
         }
 
